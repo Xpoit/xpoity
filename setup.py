c35df075bc3107cca7200f9cdd4c978299f930ea
Xpoit <Tgbhaiofficiall@gmail.com>
2024-10-29 14:09:09 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='toutatis',
+    name='xpoity',
     version="1.31",
     packages=find_packages(),
-    author="megadose",
+    author="xpoit",
     install_requires=["argparse","requests","phonenumbers","pycountry"],
     description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     long_description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     include_package_data=True,
-    url='http://github.com/megadose/toutatis',
-    entry_points = {'console_scripts': ['toutatis = toutatis.core:main']},
+    url='http://github.com/xpoit/xpoity',
+    entry_points = {'console_scripts': ['xpoity = xpoity.core:main']},
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
